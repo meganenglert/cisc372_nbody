de4ef789941436fbffd9152754045d7d308676b1
megan <menglert@udel.edu>
2023-05-12 17:01:20 -0400

    current stuff

---
+++ compute.cu
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <math.h>
+#include "vector.h"
+#include "config.h"
+
+// compute: Updates the positions and locations of the objects in the system based on gravity.
+// Parameters: None
+// Returns: None
+// Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
+__global__ void compute_help()
+{
+}
+__global__ void construct_row(int NUMENTITIES, vector3 *values, vector3 **accels)
+{
+	int index = threadIdx.x;
+	int stride = blockDim.x;
+	
+	for (int i = index; i < NUMENTITIES; i += stride)
+	{
+		accels[i] = &values[i * NUMENTITIES];
+	}
+	for (int i = index; i < NUMENTITIES; i += stride)
+	{
+		// first compute the pairwise accelerations.  Effect is on the first argument.
+		for (j = 0; j < NUMENTITIES; j++)
+		{
+			if (i == j)
+			{
+				FILL_VECTOR(accels[i][j], 0, 0, 0);
+			}
+			else
+			{
+				vector3 distance;
+				for (k = 0; k < 3; k++)
+					distance[k] = hPos[i][k] - hPos[j][k];
+				double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
+				double magnitude = sqrt(magnitude_sq);
+				double accelmag = -1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
+				FILL_VECTOR(accels[i][j], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
+			}
+		}
+	}
+}
+void compute()
+{
+	// make an acceleration matrix which is NUMENTITIES squared in size;
+	int i, j, k;
+	vector3 *values;
+	vector3 **accels;
+	cudaMallocManaged(&values, NUMENTITIES * NUMENTITIES * sizeof(vector3 *));
+	cudaMallocManaged(&accels, NUMENTITIES * sizeof(vector3 **)) int blockSize = 256;
+	int numBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
+	construct_row<<<1,256>>>(NUMENTITIES, values, accels);
+	cudaDeviceSynchronize();
+
+	// sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	for (i = 0; i < NUMENTITIES; i++)
+	{
+		vector3 accel_sum = {0, 0, 0};
+		for (j = 0; j < NUMENTITIES; j++)
+		{
+			for (k = 0; k < 3; k++)
+				accel_sum[k] += accels[i][j][k];
+		}
+		// compute the new velocity based on the acceleration and time interval
+		// compute the new position based on the velocity and time interval
+		for (k = 0; k < 3; k++)
+		{
+			hVel[i][k] += accel_sum[k] * INTERVAL;
+			hPos[i][k] = hVel[i][k] * INTERVAL;
+		}
+	}
+	cudaFree(accels);
+	cudaFree(values);
+}
