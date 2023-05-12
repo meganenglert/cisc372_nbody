2d72a96a152f5483907517e7378b80f08d0e19b2
megan <menglert@udel.edu>
2023-05-12 18:15:30 -0400

    higae

--- compute.cu
+++ compute.cu
@@ -10,7 +10,7 @@
 //__global__ void compute_help()
 //{
 //}
-__global__ void initialize(int NUMENTITIES, (void*)values,(void **)accels) {
+__global__ void initialize(vector3 *values,vector3 **accels) {
 	int index = threadIdx.x;
 	int stride = blockDim.x;
 
