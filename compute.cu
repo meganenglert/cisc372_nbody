e776d2bd19006df0060746cfb352696f9be00be1
megan <menglert@udel.edu>
2023-05-13 16:09:10 -0400

    reverting to silber code

--- compute.cu
+++ compute.cu
@@ -3,85 +3,47 @@
 #include "vector.h"
 #include "config.h"
 
-
-__global__ void setUpMatrix(vector3 *values,vector3 **accels) {
-	int idx = threadIdx.x;
-	int stride = blockDim.x;
-
-	for (int i = idx; i < NUMENTITIES; i += stride)
-	{
-		accels[i] = &values[i * NUMENTITIES];
-	}
-}
-
-__global__ void valfill(vector3 *values, vector3 **accels, vector3 *d_hPos, double* d_mass)
-{
-	int idx = threadIdx.x;
-	int stride = blockDim.x;
-	for (int i = idx; i < NUMENTITIES; i += stride)
-	{
-		// first compute the pairwise accelerations.  Effect is on the first argument.
-		for (int j = 0; j < NUMENTITIES; j++)
-		{
-			if (i == j)
-			{
-				FILL_VECTOR(accels[i][j], 0, 0, 0);
+//compute: Updates the positions and locations of the objects in the system based on gravity.
+//Parameters: None
+//Returns: None
+//Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
+void compute(){
+	//make an acceleration matrix which is NUMENTITIES squared in size;
+	int i,j,k;
+	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
+	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
+	for (i=0;i<NUMENTITIES;i++)
+		accels[i]=&values[i*NUMENTITIES];
+	//first compute the pairwise accelerations.  Effect is on the first argument.
+	for (i=0;i<NUMENTITIES;i++){
+		for (j=0;j<NUMENTITIES;j++){
+			if (i==j) {
+				FILL_VECTOR(accels[i][j],0,0,0);
 			}
-			else
-			{
+			else{
 				vector3 distance;
-				for (int k = 0; k < 3; k++)
-					distance[k] = d_hPos[i][k] - d_hPos[j][k];
-				double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
-				double magnitude = sqrt(magnitude_sq);
-				double accelmag = -1 * GRAV_CONSTANT * d_mass[j] / magnitude_sq;
-				FILL_VECTOR(accels[i][j], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
+				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+				double magnitude=sqrt(magnitude_sq);
+				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
 			}
 		}
 	}
-}
-void compute(vector3 *d_hVel, vector3 *d_hPos, double *d_mass)
-{
-	// make an acceleration matrix which is NUMENTITIES squared in size;
-	int i, j, k;
-	int blockSize = 256;
-	vector3 *values = (vector3 *)malloc(sizeof(vector3) * NUMENTITIES * NUMENTITIES);
-	vector3 **accels = (vector3 **)malloc(sizeof(vector3 *) * NUMENTITIES);
-	vector3 *d_values;
-	cudaMalloc((vector3 **)&d_values, sizeof(vector3) * NUMENTITIES * NUMENTITIES);
-	vector3 **d_accels;
-	cudaMalloc((vector3 ***)&d_accels, sizeof(vector3 *) * NUMENTITIES);
-	vector3 accel_sum;
-	int numBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
-	setUpMatrix<<<numBlocks, blockSize>>>(d_values, d_accels);
-	cudaDeviceSynchronize();
-	valfill<<<numBlocks, blockSize>>>(d_values, d_accels, d_hPos, d_mass);
-	cudaDeviceSynchronize();
-	cudaMemcpy(d_hPos, hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost);
-	cudaMemcpy(d_mass, mass, sizeof(double)*NUMENTITIES, cudaMemcpyDeviceToHost);
-
-	// sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i = 0; i < NUMENTITIES; i++)
-	{
-		vector3 accel_sum = {0, 0, 0};
-		for (j = 0; j < NUMENTITIES; j++)
-		{
-			for (k = 0; k < 3; k++)
-				accel_sum[k] += d_accels[i][j][k];
+	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	for (i=0;i<NUMENTITIES;i++){
+		vector3 accel_sum={0,0,0};
+		for (j=0;j<NUMENTITIES;j++){
+			for (k=0;k<3;k++)
+				accel_sum[k]+=accels[i][j][k];
 		}
-		// compute the new velocity based on the acceleration and time interval
-		// compute the new position based on the velocity and time interval
-		for (k = 0; k < 3; k++)
-		{
-			d_hVel[i][k] += accel_sum[k] * INTERVAL;
-			d_hPos[i][k] = d_hVel[i][k] * INTERVAL;
+		//compute the new velocity based on the acceleration and time interval
+		//compute the new position based on the velocity and time interval
+		for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]=hVel[i][k]*INTERVAL;
 		}
 	}
-	cudaMemcpy(d_hPos, hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost);
-	cudaMemcpy(d_hVel, hVel, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost);
-	cudaMemcpy(d_mass, mass, sizeof(double)*NUMENTITIES, cudaMemcpyDeviceToHost);
 	free(accels);
 	free(values);
-	cudaFree(d_accels);
-	cudaFree(d_values);
 }
--- nbody.cu
+++ nbody.cu
@@ -10,12 +10,12 @@
 // represents the objects in the system.  Global variables
 vector3 *hVel, *d_hVel;
 vector3 *hPos, *d_hPos;
-double *mass, *d_mass;
+double *mass;
 
-// initHostMemory: Create storage for numObjects entities in our system
-// Parameters: numObjects: number of objects to allocate
-// Returns: None
-// Side Effects: Allocates memory in the hVel, hPos, and mass global variables
+//initHostMemory: Create storage for numObjects entities in our system
+//Parameters: numObjects: number of objects to allocate
+//Returns: None
+//Side Effects: Allocates memory in the hVel, hPos, and mass global variables
 void initHostMemory(int numObjects)
 {
 	hVel = (vector3 *)malloc(sizeof(vector3) * numObjects);
@@ -23,11 +23,10 @@ void initHostMemory(int numObjects)
 	mass = (double *)malloc(sizeof(double) * numObjects);
 }
 
-
-// freeHostMemory: Free storage allocated by a previous call to initHostMemory
-// Parameters: None
-// Returns: None
-// Side Effects: Frees the memory allocated to global variables hVel, hPos, and mass.
+//freeHostMemory: Free storage allocated by a previous call to initHostMemory
+//Parameters: None
+//Returns: None
+//Side Effects: Frees the memory allocated to global variables hVel, hPos, and mass.
 void freeHostMemory()
 {
 	free(hVel);
@@ -35,54 +34,22 @@ void freeHostMemory()
 	free(mass);
 }
 
-void freeDeviceMemory()
-{
-	cudaFree(d_hVel);
-	cudaFree(d_hPos);
-	cudaFree(d_mass);
-}
-
-// planetFill: Fill the first NUMPLANETS+1 entries of the entity arrays with an estimation
+//planetFill: Fill the first NUMPLANETS+1 entries of the entity arrays with an estimation
 //				of our solar system (Sun+NUMPLANETS)
-// Parameters: None
-// Returns: None
-// Fills the first 8 entries of our system with an estimation of the sun plus our 8 planets.
-void planetFill()
-{
-	int i, j;
-	double data[][7] = {SUN, MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE};
-	for (i = 0; i <= NUMPLANETS; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			hPos[i][j] = data[i][j];
-			hVel[i][j] = data[i][j + 3];
+//Parameters: None
+//Returns: None
+//Fills the first 8 entries of our system with an estimation of the sun plus our 8 planets.
+void planetFill(){
+	int i,j;
+	double data[][7]={SUN,MERCURY,VENUS,EARTH,MARS,JUPITER,SATURN,URANUS,NEPTUNE};
+	for (i=0;i<=NUMPLANETS;i++){
+		for (j=0;j<3;j++){
+			hPos[i][j]=data[i][j];
+			hVel[i][j]=data[i][j+3];
 		}
-		mass[i] = data[i][6];
+		mass[i]=data[i][6];
 	}
 }
-
-// randomFill: FIll the rest of the objects in the system randomly starting at some entry in the list
-// Parameters: 	start: The index of the first open entry in our system (after planetFill).
-//				count: The number of random objects to put into our system
-// Returns: None
-// Side Effects: Fills count entries in our system starting at index start (0 based)
-/*__global__ void randomFill(vector3 *d_hVel, vector3 *d_hPos, double *d_mass)
-{
-	int i, j;
-	int start = threadIdx.x * blockIdx.x + 7;
-	int count = blockDim.x;
-	for (i = start; i < start + count + 7; i++)
-	{
-		if (i < NUMENTITIES) {
-		for (j = 0; j < 3; j++)
-		{
-			d_hVel[i][j] = (double)curand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
-			d_hPos[i][j] = (double)curand() / RAND_MAX * MAX_VELOCITY * 2 - MAX_VELOCITY;
-			d_mass[i] = (double)curand() / RAND_MAX * MAX_MASS;
-		}}
-	}
-}*/
 
 //randomFill: FIll the rest of the objects in the system randomly starting at some entry in the list
 //Parameters: 	start: The index of the first open entry in our system (after planetFill).
@@ -91,10 +58,9 @@ void planetFill()
 //Side Effects: Fills count entries in our system starting at index start (0 based)
 void randomFill(int start, int count)
 {
-	int i, j;
+	int i, j, c = start;
 	for (i = start; i < start + count; i++)
 	{
-		printf("filling %d\n", i);
 		for (j = 0; j < 3; j++)
 		{
 			hVel[i][j] = (double)rand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
@@ -104,89 +70,46 @@ void randomFill(int start, int count)
 	}
 }
 
-// printSystem: Prints out the entire system to the supplied file
-// Parameters: 	handle: A handle to an open file with write access to prnt the data to
-// Returns: 		none
-// Side Effects: Modifies the file handle by writing to it.
-void printSystem(FILE *handle)
-{
-	int i, j;
-	for (i = 0; i < NUMENTITIES; i++)
-	{
-		fprintf(handle, "pos=(\n");
-		fflush(stdout);
-		for (j = 0; j < 3; j++)
-		{
-			fprintf(handle, "%lf,\n", hPos[i][j]);
-			fflush(stdout);
+//printSystem: Prints out the entire system to the supplied file
+//Parameters: 	handle: A handle to an open file with write access to prnt the data to
+//Returns: 		none
+//Side Effects: Modifies the file handle by writing to it.
+void printSystem(FILE* handle){
+	int i,j;
+	for (i=0;i<NUMENTITIES;i++){
+		fprintf(handle,"pos=(");
+		for (j=0;j<3;j++){
+			fprintf(handle,"%lf,",hPos[i][j]);
 		}
-		printf("),v=(\n");
-		fflush(stdout);
-		for (j = 0; j < 3; j++)
-		{
-			fprintf(handle, "%lf,\n", hVel[i][j]);
-			fflush(stdout);
+		printf("),v=(");
+		for (j=0;j<3;j++){
+			fprintf(handle,"%lf,",hVel[i][j]);
 		}
-		fprintf(handle, "),m=%lf\n", mass[i]);
-		fflush(stdout);
-		printf("OBJECT NUMBER %d\n", i);
-		fflush(stdout);
+		fprintf(handle,"),m=%lf\n",mass[i]);
 	}
-}
-
-void devToHost() {
-	cudaMemcpy(d_hPos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
-	cudaMemcpy(d_hVel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
-	cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyDeviceToHost);
-}
-
-void hostToDev() {
-	cudaMemcpy(d_hPos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_hVel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice);
 }
 
 int main(int argc, char **argv)
 {
-	clock_t t0 = clock();
+	clock_t t0=clock();
 	int t_now;
-	// srand(time(NULL));
+	//srand(time(NULL));
 	srand(1234);
-	fflush(stdout);
 	initHostMemory(NUMENTITIES);
 	planetFill();
-	printf("planet filled\n");
-	fflush(stdout);
 	randomFill(NUMPLANETS + 1, NUMASTEROIDS);
-	
-	//DEVICE ----------------------------------------------------
-	cudaMalloc((void **)&d_hVel, sizeof(vector3) * NUMENTITIES);
-	cudaMalloc((void **)&d_hPos, (size_t)sizeof(vector3) * NUMENTITIES);
-	cudaMalloc((void **)&d_mass, sizeof(double)*NUMENTITIES);
-	cudaMemcpy( d_hVel,hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_hPos,hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_mass, mass,sizeof(double)*NUMENTITIES, cudaMemcpyHostToDevice);
-	// --------------------------------------------------------------------
-	printf("here\n");
-// now we have a system.
-#ifdef DEBUG
+	//now we have a system.
+	#ifdef DEBUG
 	printSystem(stdout);
-#endif
-fflush(stdout);
-printf("here\n");
-fflush(stdout);
-	for (t_now = 0; t_now < DURATION; t_now += INTERVAL)
-	{
-		compute(d_hVel, d_hPos, d_mass);
+	#endif
+	for (t_now=0;t_now<DURATION;t_now+=INTERVAL){
+		compute();
 	}
-	
-	devToHost();
-	clock_t t1 = clock() - t0;
+	clock_t t1=clock()-t0;
 #ifdef DEBUG
 	printSystem(stdout);
 #endif
-	printf("This took a total time of %f seconds\n", (double)t1 / CLOCKS_PER_SEC);
+	printf("This took a total time of %f seconds\n",(double)t1/CLOCKS_PER_SEC);
 
 	freeHostMemory();
-	freeDeviceMemory();
 }
