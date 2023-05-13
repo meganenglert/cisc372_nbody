e6591072895266ad83c4fac85fde73a5c9c64e45
megan <menglert@udel.edu>
2023-05-12 22:00:58 -0400

    ~

--- compute.cu
+++ compute.cu
@@ -77,7 +77,9 @@ void compute(vector3 *d_hVel, vector3 *d_hPos, double *d_mass)
 			d_hPos[i][k] = d_hVel[i][k] * INTERVAL;
 		}
 	}
-	
+	cudaMemcpy(d_hPos, hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost);
+	cudaMemcpy(d_hVel, hVel, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost);
+	cudaMemcpy(d_mass, mass, sizeof(double)*NUMENTITIES, cudaMemcpyDeviceToHost);
 	free(accels);
 	free(values);
 	cudaFree(d_accels);
