ba0413e31e264469db2131b84db8de33be6174fe
megan <menglert@udel.edu>
2023-05-12 20:40:35 -0400

    ~

--- compute.cu
+++ compute.cu
@@ -65,11 +65,11 @@ void compute(vector3 *d_hVel, vector3 *d_hPos, double *d_mass)
 	// sum up the rows of our matrix to get effect on each entity, then update velocity and position.
 	for (i = 0; i < NUMENTITIES; i++)
 	{
-		(void*)d_accel_sum = {0, 0, 0};
+		d_accel_sum = {0, 0, 0};
 		for (j = 0; j < NUMENTITIES; j++)
 		{
 			for (k = 0; k < 3; k++)
-				d_accel_sum[k] += accels[i][j][k];
+				d_accel_sum[k] += d_accels[i][j][k];
 		}
 		// compute the new velocity based on the acceleration and time interval
 		// compute the new position based on the velocity and time interval
